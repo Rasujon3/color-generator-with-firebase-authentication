f7a603cff50dc13561b551b4abf00fd57f1c1a27
Rasujon3 <rasujon3@gmail.com>
2022-04-15 14:05:46 +0600

    firebase add done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAHRnDWhQ8tRq3dHOcNZ95wuit9vrH7bRI",
-    authDomain: "ranco-a0bb5.firebaseapp.com",
-    projectId: "ranco-a0bb5",
-    storageBucket: "ranco-a0bb5.appspot.com",
-    messagingSenderId: "775789481609",
-    appId: "1:775789481609:web:4b3555c113878daf944058",
+  apiKey: "AIzaSyD_nxQPgmlRY-5b911iyN2TR00utbUiMgI",
+  authDomain: "color-generator-with-fb-auth.firebaseapp.com",
+  projectId: "color-generator-with-fb-auth",
+  storageBucket: "color-generator-with-fb-auth.appspot.com",
+  messagingSenderId: "2233912649",
+  appId: "1:2233912649:web:e2320965c535172ae19e98",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
