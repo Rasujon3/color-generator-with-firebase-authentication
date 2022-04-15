33d38547dc49156f0641cba70ed5e59a2eae9c6a
Rasujon3 <rasujon3@gmail.com>
2022-04-15 14:45:52 +0600

    email validation done

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,21 +1,37 @@
 import React, { useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "../../../firebase.init";
 import "../../../styles/Login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const [signInWithEmailAndPassword, user, loading, hookError] =
+    useSignInWithEmailAndPassword(auth);
 
   const handleEmailChange = (e) => {
-    setEmail(e.target.value);
+    const emailregex = /\S+@\S+\.\S+/;
+    const validEmail = emailregex.test(e.target.value);
+    console.log(validEmail);
+
+    // setEmail(e.target.value);
   };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(email, password);
+  };
+
   return (
     <div className="login-container">
       <div className="login-title">LOGIN</div>
-      <form className="login-form">
+      <form onSubmit={handleLogin} className="login-form">
         <input
           type="text"
           placeholder="Your Email"
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
